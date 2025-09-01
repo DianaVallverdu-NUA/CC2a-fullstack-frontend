c1edac3c2cbb3a28377f38a674c20504fa34a0a5
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-09-01 16:47:56 +0100

    ya

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Route } from "./+types/home";
 import {Card, Button} from "antd";
 
@@ -12,15 +12,34 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
 
   const [counter, setCounter] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCounter = () => {
+    fetch("http://localhost:3000/counter").then((res) => {
+      res.text().then(text => {
+        setCounter(Number(text));
+      })
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  useEffect(() => {
+    fetchCounter();
+    let id = setInterval(fetchCounter, 500);
+    return () => clearInterval(id);
+  }, [])
 
   const addOne = () => {
+    setLoading(true);
     setCounter(counter + 1);
+    fetch("http://localhost:3000/counter", {method: "POST"}).then(() => {setLoading(false)});
   }
 
   return <div className="flex items-center justify-center w-screen h-screen">
     <Card>
       <p>Counter: {counter}</p>
-    <Button onClick={addOne}>Add One</Button>
+    <Button onClick={addOne} disabled={loading}>Add One</Button>
     </Card>
     </div>
 }
